55bba153a6cf7f53bb275831e7c358fb4c3dbfa1
Destiro <debruconn@ecs.vuw.ac.nz>
2021-01-29 13:01:55 +1300

    Potential Screen nav fix

--- App.js
+++ App.js
@@ -6,7 +6,53 @@
  * @flow strict-local
  */
 
-import React from 'react';
+
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('Details')}
+            />
+        </View>
+    );
+}
+
+function DetailsScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+            <Button
+                title="Go to Details... again"
+                onPress={() => navigation.navigate('Details')}
+            />
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
+
+
+/*import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -75,6 +121,8 @@ const App: () => React$Node = () => {
   );
 };
 
+
+
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
@@ -114,4 +162,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default App;*/
