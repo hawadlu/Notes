1d43744b3dc5adad3d2206077756f524412b228f
Luke <hawadlu@outlook.com>
2021-02-06 15:07:21 +1300

    Added js file.

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import {Button, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+// import {NotesView} from 'src/components';
 
 function HomeScreen({navigation}) {
     return (
