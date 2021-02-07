2f498f24e2650ac322a659b339058b992354f58c
Destiro <connordebruyn234@gmail.com>
2021-02-07 16:17:53 +1300

    Partial home screen completion

--- App.js
+++ App.js
@@ -8,23 +8,39 @@
 
 
 import * as React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, StyleSheet, Dimensions, ScrollView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import NotesView from "./src/components/Notes";
 import Luke from "./src/components/Luke";
 
+/**
+ * Home screen - Select a note to edit, open selected note.
+ *
+ * @param navigation
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function HomeScreen({navigation}) {
     return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Home Screen</Text>
-            <Luke> </Luke>
+        <View style={homeStyles.container}>
+            <View style={homeStyles.div_top}>
+                <Text>Home Screen</Text>
+                <Luke> </Luke>
+            </View>
+            <View style={homeStyles.div_middle}>
             <Button
                 title="Add Note"
                 onPress={() => navigation.navigate('Details')}
             />
-        </View>
+            </View>
+            <View style={homeStyles.div_bottom}>
+                <ScrollView>
+                    <Text>Hello</Text>
+                </ScrollView>
+            </View>
+            </View>
     );
 }
 
@@ -67,6 +83,36 @@ function App() {
         </NavigationContainer>
     );
 }
+
+/**
+ * Height variable for screen.
+ */
+var {height} = Dimensions.get('window');
+var eighth_height = height/8;
+
+/**
+ * Styles for the home screen.
+ */
+const homeStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    div_top: {
+        height: eighth_height,
+        backgroundColor: '#2196F3'
+    },
+    div_middle: {
+        height: eighth_height,
+        backgroundColor: '#8BC34A'
+    },
+    div_bottom: {
+        height: eighth_height*4,
+        backgroundColor: '#e3aa1a'
+    }
+});
 
 export default App;
 
